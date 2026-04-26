ea1c6d5fde7d287eaf0c54a7001e9334a5c42269
Joe Keisker <jkeisker@protonmail.com>
2026-04-26 17:02:38 -0500

    first commit

---
+++ BestStoreMVC/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly decimal _shippingFee;
+
+        public CartController(ApplicationDbContext context, IConfiguration configuration,
+            UserManager<ApplicationUser> userManager)
+        {
+            this._context = context;
+            this._userManager = userManager;
+            this._shippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+        }
+        public IActionResult Index()
+        {
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, _context);
+            decimal subtotal = CartHelper.GetSubtotal(cartItems);
+
+            ViewBag.CartItems = cartItems;
+            ViewBag.ShippingFee = _shippingFee;
+            ViewBag.Subtotal = subtotal;
+            ViewBag.Total = subtotal + _shippingFee;
+
+            return View();
+        }
+    }
+}
---
+++ BestStoreMVC/Services/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿namespace BestStoreMVC.Services
+{
+    using sib_api_v3_sdk.Api;
+    using sib_api_v3_sdk.Model;
+
+    public class EmailSender
+    {
+        public static void SendEmail(string senderName, string senderEmail, string toName, string toEmail,
+            string subject, string textContent)
+        {
+            var apiInstance = new TransactionalEmailsApi();
+            SendSmtpEmailSender Email = new SendSmtpEmailSender(senderName, senderEmail);
+            SendSmtpEmailTo smtpEmailTo = new SendSmtpEmailTo(toEmail, toName);
+            List<SendSmtpEmailTo> To = new List<SendSmtpEmailTo>();
+            To.Add(smtpEmailTo);
+
+            try
+            {
+                var sendSmtpEmail = new SendSmtpEmail(Email, To, null, null, null, textContent, subject);
+                CreateSmtpEmail result = apiInstance.SendTransacEmail(sendSmtpEmail);
+                Console.WriteLine("Email Sender OK: \n" + result.ToJson());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Email Sender Failure: \n" + ex.Message);
+            }
+        }
+    }
+}
